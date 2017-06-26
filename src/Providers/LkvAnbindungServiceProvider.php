e3e081c397f907a5191f6f575775c7961a108d21
Michael Csáki <m.csaki@mlc-media.com>
2017-06-26 12:23:28 +0200

    Fix Cron

--- src/Providers/LkvAnbindungServiceProvider.php
+++ src/Providers/LkvAnbindungServiceProvider.php
@@ -6,6 +6,7 @@ use Plenty\Plugin\ServiceProvider;
 use Plenty\Modules\Cron\Services\CronContainer;
 use Plenty\Modules\EventProcedures\Services\EventProceduresService;
 use Plenty\Log\Services\ReferenceContainer;
+use LkvAnbindung\Crons\LkvOrderExport;
  
 class LkvAnbindungServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,6 @@ class LkvAnbindungServiceProvider extends ServiceProvider
     }
 
     public function boot(CronContainer $container, EventProceduresService $eventProceduresService, ReferenceContainer $referenceContainer) {
-        $container->add(CronContainer::EVERY_FITEEN_MINUTES, )
+        $container->add(CronContainer::EVERY_FITEEN_MINUTES, LkvOrderExport::class);
     }
 }
